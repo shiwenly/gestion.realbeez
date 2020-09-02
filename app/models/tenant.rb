c610f0563751fe0bb38c6e3e6235a9f7422b8344
steven ly <shiwenly@gmail.com>
2020-09-02 16:03:16 +0200

    add picture

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -3,8 +3,11 @@ class Tenant < ApplicationRecord
   belongs_to :apartment
   has_many :waters, dependent: :destroy
   has_many :rents, dependent: :destroy
-  has_one_attached :contract
-  has_one_attached :inventory
+  # has_one_attached :contract
+  # has_one_attached :inventory
+
+  mount_uploader :contract, PhotoUploader
+  mount_uploader :inventory, PhotoUploader
 
   validates :last_name, :rent, :service_charge, presence: true
 end
--- app/models/water.rb
+++ app/models/water.rb
@@ -2,5 +2,6 @@ class Water < ApplicationRecord
   belongs_to :user
   belongs_to :tenant
 
-  has_one_attached :photo
+  # has_one_attached :photo
+  mount_uploader :photo, PhotoUploader
 end
